9022349be92f5635fa51dfc2208d549dae288e9e
SIDDHARTH BHARDWAJ <siddharthkumar538@gmail.com>
2021-06-28 21:35:53 +0530

    Initial commit

---
+++ src/testcase/testclass.java
@@ -0,0 +1,22 @@
+package testcase;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.chrome.ChromeDriver; 
+import org.openqa.selenium.support.ui.WebDriverWait;
+public class testclass {
+	public static void main(String[] args)
+	{
+		System.setProperty("webdriver.chrome.driver","H:\\chromedriver.exe");
+		WebDriver driver =new ChromeDriver();
+		driver.get("https://www.google.com");
+		
+		
+		WebElement search = driver.findElement(By.name("q"));
+		search.sendKeys("Siddharth");
+		
+	}
+}
