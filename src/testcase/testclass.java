643179ea2c434253564ed9455dea972984a84ebc
SIDDHARTH BHARDWAJ <siddharthkumar538@gmail.com>
2021-06-29 09:58:12 +0530

    Remove unused imports.

--- src/testcase/testclass.java
+++ src/testcase/testclass.java
@@ -4,9 +4,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.chrome.ChromeDriver; 
-import org.openqa.selenium.support.ui.WebDriverWait;
 public class testclass {
 	public static void main(String[] args)
 	{
